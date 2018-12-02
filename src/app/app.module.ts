79803344eff0bfd7da9d8be8546124044d47f29f
manumohan009 <manumohan.cvav@gmail.com>
2018-12-02 20:16:45 +0530

    enabled DevToolsExtension for redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { NgRedux, NgReduxModule } from '@angular-redux/store';
+import { NgRedux, NgReduxModule, DevToolsExtension  } from '@angular-redux/store';
 import { IAppState, rootReducer, INITIAL_STATE } from './store';
 
 
 import { AppComponent } from './app.component';
 import { TodoOverviewComponent } from './todo-overview/todo-overview.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,12 @@ import { TodoListComponent } from './todo-list/todo-list.component';
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(ngRedux: NgRedux<IAppState>) {
-    ngRedux.configureStore(rootReducer, INITIAL_STATE);
+  constructor(ngRedux: NgRedux<IAppState>, private devTools: DevToolsExtension) {
+
+    let enhancers = [];
+    if (environment.production === false && devTools.isEnabled() {
+      enhancers = [ ...enhancers, devTools.enhancer() ];
+    }
+    ngRedux.configureStore(rootReducer, INITIAL_STATE,[], enhancers);
   }
  }
